3870f1ae90c6661a33624c3d958322fab1fc8411
monse45 <160539864+mons45@users.noreply.github.com>
2025-01-19 14:31:19 +0100

    change fonctions

--- src/Recipe.jsx
+++ src/Recipe.jsx
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 import RecipeCard from "./RecipeCard";
 
 const Recipe = ({ recipes }) => {
-    if (recipes.length){
-        return(
-            <h1 className="text-zinc-300 text-center text-lg m-56">We re sorry name not found try another name</h1>
-        )
-    }
+  if (recipes.length === 0) {
+    return (
+      <h1 className="text-zinc-300 text-center text-lg m-56">
+        We’re sorry, no recipes found. Try another search term!
+      </h1>
+    );
+  }
   return (
     <div className="cards grid grid-cols-3 gap-4">
       {recipes.map((recipe, index) => (
@@ -19,3 +21,4 @@ const Recipe = ({ recipes }) => {
 };
 
 export default Recipe;
+
--- src/RecipeDetails.jsx
+++ src/RecipeDetails.jsx
@@ -1,8 +1,8 @@
 import { useParams } from "react-router-dom";
 
 const RecipeDetails = ({ recipes }) => {
-  const { id } = useParams(); // Get the recipe ID from the URL
-  const recipe = recipes[id]; // Retrieve the recipe using the ID
+  const { id } = useParams(); 
+  const recipe = recipes[id]; 
 
   if (!recipe) {
     return <div>Recipe not found!</div>;
@@ -10,18 +10,22 @@ const RecipeDetails = ({ recipes }) => {
 
   return (
     <div className="recipe-details max-w-4xl mx-auto p-6">
+          <img className=" w-40 h-40 mt-14" src={recipe.image} alt={recipe.label} height="351px" />
+          <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">{recipe.label}</div>
+    </div>
       <h1 className="text-3xl font-bold text-gray-800">{recipe.title}</h1>
-      <p className="text-gray-600 mt-4">{recipe.description}</p>
+      <p className="text-gray-300 mt-4">{recipe.description}</p>
 
       <h2 className="text-2xl font-semibold mt-6 mb-4">Ingredients:</h2>
-      <ul className="list-disc list-inside text-gray-700 text-base">
+      <ul className="list-disc list-inside text-gray-300 text-base">
         {recipe.ingredients.map((ingredient, index) => (
           <li key={index}>{ingredient.text}</li>
         ))}
       </ul>
 
       <h2 className="text-2xl font-semibold mt-6 mb-4">Instructions:</h2>
-      <p className="text-gray-700 text-base">{recipe.instructions}</p>
+      <p className="text-gray-300 text-base">{recipe.instructions}</p>
     </div>
   );
 };
